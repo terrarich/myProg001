2aefbc03e5b4bb4486ff1152a90ea4d2cdecff14
Terrarich <163340619+terrarich@users.noreply.github.com>
2025-02-26 15:46:30 +0300

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,19 @@
+fn main(){
+    
+    let a: i16 = 11;
+    
+    if a < 10{
+        println!("a is not 10");
+        println!("a = {a}");
+    }else if a > 10 {
+        println!("a is not 10");
+        println!("a = {a}");
+    }else if a == 10{
+        println!("a = 10");
+    }
+    fin();
+}
+
+fn fin(){
+    println!("PROGRAMM  FINISHED");
+}
